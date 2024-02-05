eb54e45f25a3329189986f4f7e00fb058bbfd35e
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2024-02-05 13:48:24 +0100

    C# Added

---
+++ 01._Data_Files/FileBonanzaCSharp/FileBonanzaCSharp/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileBonanzaCSharp
+{
+	public class Person
+	{
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public string Hobbies { get; set; }
+
+		public override string ToString()
+		{
+			return $"Name: {Name}, Age: {Age}, Country: {Country}, Hobbies: {Hobbies}";
+		}
+	}
+}
---
+++ 01._Data_Files/FileBonanzaCSharp/FileBonanzaCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using FileBonanzaCSharp;
+using System.Globalization;
+
+var csvFilePath = "C:\\KEA\\System Integration\\SystemIntegrationAssignments\\01._Data_Files\\me.csv";
+
+var csvHelperConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+	PrepareHeaderForMatch = args => args.Header.ToLower(),
+	Delimiter = ";"
+};
+
+using (var reader = new StreamReader(csvFilePath))
+using (var csv = new CsvReader(reader, csvHelperConfig))
+{
+	var records = csv.GetRecords<Person>();
+	foreach (var r in records)
+	{
+		Console.WriteLine(r);
+	}
+}
