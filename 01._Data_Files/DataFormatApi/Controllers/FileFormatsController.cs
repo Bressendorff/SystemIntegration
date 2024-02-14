4b8b537448add6f2f8aabee8b4e8bf7dc3fe357d
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2024-02-14 20:20:56 +0100

    API with endpoints for parsed CSV, Txt, JSON and XML files

---
+++ 01._Data_Files/DataFormatApi/Controllers/FileFormatsController.cs
@@ -0,0 +1,39 @@
+﻿using FileBonanzaCSharp;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DataFormatApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FileFormatsController : ControllerBase
+	{
+		// GET api/<FileFormatsController>/csv
+		[HttpGet("/csv")]
+		public ActionResult<List<Person>> GetCsv()
+		{
+			return Ok(Parser.ReadCsv());
+		}
+
+		[HttpGet("/txt")]
+		public ActionResult<string> GetTxt()
+		{
+			return Ok(Parser.ReadTxt());
+		}
+
+		[HttpGet("/json")]
+		public ActionResult<List<Person>> GetJson()
+		{
+			return Ok(Parser.ReadJson());
+		}
+
+		[HttpGet("/xaml")]
+		public ActionResult<List<Person>> GetXaml()
+		{
+			return Ok(Parser.ReadCsv());
+		}
+
+
+	}
+}
