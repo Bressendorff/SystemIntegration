5df1ba337e10e0583570235442cfe73d0db38d51
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2024-02-14 18:23:05 +0100

    Updated FileReading to happen in distinct methods in a parser class

---
+++ 01._Data_Files/FileBonanzaCSharp/FileBonanzaCSharp/Parser.cs
@@ -0,0 +1,94 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Xml;
+
+namespace FileBonanzaCSharp
+{
+	public static class Parser
+	{
+		private static string _basePath { get; set; } = "C:\\KEA\\System Integration\\SystemIntegrationAssignments\\01._Data_Files\\me";
+
+		public static string ReadTxt()
+		{
+			var path = _basePath + ".txt";
+			using var reader = new StreamReader(path);
+			var txt = reader.ReadToEnd();
+			return txt;
+		}
+
+		public static List<Person> ReadCsv()
+		{
+			var path = _basePath + ".csv";
+
+			var csvHelperConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+			{
+				PrepareHeaderForMatch = args => args.Header.ToLower(),
+				Delimiter = ";"
+			};
+
+			using var reader = new StreamReader(path);
+			using var csv = new CsvReader(reader, csvHelperConfig);
+			var people = csv.GetRecords<Person>().ToList();
+
+			return people;
+		}
+
+		public static List<Person> ReadJson()
+		{
+			var path = _basePath + ".json";
+			using var reader = new StreamReader(path);
+			string json = reader.ReadToEnd();
+			dynamic array = JsonConvert.DeserializeObject(json);
+			var people = new List<Person>();
+			foreach (var item in array)
+			{
+				Person person = new() { Age = item.Age, Country = item.Country, Name = item.Name, Hobbies = item.Hobbies.ToString().Replace("\r\n", "").Replace(" ", "") };
+				people.Add(person);
+			}
+
+			return people;
+		}
+
+		public static List<Person> ReadXml()
+		{
+			var path = _basePath + ".xml";
+
+			var doc = new XmlDocument();
+			doc.Load(path);
+			Person p = new();
+			foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+			{
+				if (node.Name == "hobbies")
+				{
+					var hobbiesString = "";
+					foreach (XmlNode hobby in node.ChildNodes)
+					{
+						hobbiesString += hobby.InnerText + ", ";
+					}
+					hobbiesString = hobbiesString.TrimEnd().TrimEnd(',');
+					p.Hobbies = hobbiesString;
+				}
+				else if (node.Name == "age")
+				{
+					p.Age = int.Parse(node.InnerText);
+				}
+				else if (node.Name == "name")
+				{
+					p.Name = node.InnerText;
+				}
+				else if (node.Name == "country")
+				{
+					p.Country = node.InnerText;
+				}
+			}
+			return new List<Person>() { p };
+		}
+	}
+}
--- 01._Data_Files/FileBonanzaCSharp/FileBonanzaCSharp/Program.cs
+++ 01._Data_Files/FileBonanzaCSharp/FileBonanzaCSharp/Program.cs
@@ -7,80 +7,93 @@ using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 using System.Xml;
 using System.Xml.Serialization;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
-var filePath = "C:\\KEA\\System Integration\\SystemIntegrationAssignments\\01._Data_Files\\me.";
-var filePaths = new List<string>() { filePath + "csv", filePath + "json", filePath + "xml" };
+//var filePath = "C:\\KEA\\System Integration\\SystemIntegrationAssignments\\01._Data_Files\\me.";
 
-var rand = new Random();
-var selectedFilePath = filePaths[rand.Next(0, filePaths.Count - 1)];
+//var filePaths = new List<string>() { filePath + "csv", filePath + "json", filePath + "xml", filePath + "yaml", filePath + "txt" };
 
-List<Person> people = new();
+//var rand = new Random();
+//var selectedFilePath = filePaths[rand.Next(0, filePaths.Count - 1)];
 
-switch (selectedFilePath.Replace(filePath, ""))
-{
-	case "csv":
-		var csvHelperConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
-		{
-			PrepareHeaderForMatch = args => args.Header.ToLower(),
-			Delimiter = ";"
-		};
+//List<Person> people = Parser.ReadJson();
 
-		using (var reader = new StreamReader(selectedFilePath))
-		using (var csv = new CsvReader(reader, csvHelperConfig))
-		{
-			people = csv.GetRecords<Person>().ToList();
-		}
-		break;
-	case "json":
-		using (var reader = new StreamReader(selectedFilePath))
-		{
-			string json = reader.ReadToEnd();
-			dynamic array = JsonConvert.DeserializeObject(json);
-			people = new List<Person>();
-			foreach (var item in array)
-			{
-				Person person = new() { Age = item.Age, Country = item.Country, Name = item.Name, Hobbies = item.Hobbies.ToString() };
-				people.Add(person);
-			}
-		}
-		break;
-	case "xml":
-		var doc = new XmlDocument();
-		doc.Load(selectedFilePath);
-		Person p = new();
-		foreach (XmlNode node in doc.DocumentElement.ChildNodes)
-		{
-			if (node.Name == "hobbies")
-			{
-				var hobbiesString = "";
-				foreach (XmlNode hobby in node.ChildNodes)
-				{
-					hobbiesString += hobby.InnerText + ", ";
-				}
-				hobbiesString = hobbiesString.TrimEnd().TrimEnd(',');
-				p.Hobbies = hobbiesString;
-			}
-			else if (node.Name == "age")
-			{
-				p.Age = int.Parse(node.InnerText);
-			}
-			else if (node.Name == "name")
-			{
-				p.Name = node.InnerText;
-			}
-			else if (node.Name == "country")
-			{
-				p.Country = node.InnerText;
-			}
-		}
-		people.Add(p);
-		break;
-	default:
-		break;
-}
+//switch (selectedFilePath.Replace(filePath, ""))
+//{
+//	case "csv":
+//		var csvHelperConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+//		{
+//			PrepareHeaderForMatch = args => args.Header.ToLower(),
+//			Delimiter = ";"
+//		};
+
+//		using (var reader = new StreamReader(selectedFilePath))
+//		using (var csv = new CsvReader(reader, csvHelperConfig))
+//		{
+//			people = csv.GetRecords<Person>().ToList();
+//		}
+//		break;
+//	case "json":
+//		using (var reader = new StreamReader(selectedFilePath))
+//		{
+//			string json = reader.ReadToEnd();
+//			dynamic array = JsonConvert.DeserializeObject(json);
+//			people = new List<Person>();
+//			foreach (var item in array)
+//			{
+//				Person person = new() { Age = item.Age, Country = item.Country, Name = item.Name, Hobbies = item.Hobbies.ToString() };
+//				people.Add(person);
+//			}
+//		}
+//		break;
+//	case "xml":
+//		var doc = new XmlDocument();
+//		doc.Load(selectedFilePath);
+//		Person p = new();
+//		foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+//		{
+//			if (node.Name == "hobbies")
+//			{
+//				var hobbiesString = "";
+//				foreach (XmlNode hobby in node.ChildNodes)
+//				{
+//					hobbiesString += hobby.InnerText + ", ";
+//				}
+//				hobbiesString = hobbiesString.TrimEnd().TrimEnd(',');
+//				p.Hobbies = hobbiesString;
+//			}
+//			else if (node.Name == "age")
+//			{
+//				p.Age = int.Parse(node.InnerText);
+//			}
+//			else if (node.Name == "name")
+//			{
+//				p.Name = node.InnerText;
+//			}
+//			else if (node.Name == "country")
+//			{
+//				p.Country = node.InnerText;
+//			}
+//		}
+//		people.Add(p);
+//		break;
+//	case "yaml" or "txt":
+//		using (var reader = new StreamReader(selectedFilePath))
+//		{
+//			var yaml = reader.ReadToEnd();
+//			Console.WriteLine(yaml);
+//		}
+//		break;
+//	default:
+//		break;
+//}
 
 
-foreach (var person in people)
-{
-	Console.WriteLine(person);
-}
+//foreach (var person in people)
+//{
+//	Console.WriteLine(person);
+//}
+
+Console.WriteLine();
