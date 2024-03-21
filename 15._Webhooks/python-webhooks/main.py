a3e5ac81e3f686ed77b60dedb8e9629f06152fe4
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2024-03-21 08:30:32 +0100

    SSE, Websockets and using webhooks

---
+++ 15._Webhooks/python-webhooks/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request, Response
+import json
+
+app = FastAPI()
+
+@app.post("/githubwebhookjson")
+async def github_webhook(request: Request, response: Response):
+    if request.headers.get("content-type") == "application/json":
+        data = await request.body()
+        print(json.loads(data))
+        response.status_code = 200
+    else:
+        response.status_code = 400
+    
+@app.post("/githubwebhookform")
+async def github_webhook(request: Request, response: Response):
+    if request.headers.get("content-type") == "application/x-www-form-urlencoded":
+        form_data = await request.form()
+        payload = form_data['payload']
+        print(payload)
+        response.status_code = 200
+    else:
+        response.status_code = 400
+    
